d070ce967eb2c0a7b79c177c96ae060616d4a4a8
zhaoxh5 <67985916+zhaoxh5@users.noreply.github.com>
2020-11-30 00:14:28 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -21,10 +21,6 @@ sed -i 's/OpenWrt/OpenWrt-N1/g' ./package/base-files/files/bin/config_generate
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 
-# 修改banne文件（banne文件在根目录）（不要修改此行代码,怕弄的diy-lede.sh文件全失效,不需要的话前面加#，或者全行代码删除了）
-#rm -rf ./package/base-files/files/etc/banne && cd .. && cp -f ./banner openwrt/package/base-files/files/etc/ && cd openwrt
-
-
 #内核版本是会随着源码更新而改变的，在coolsnowwolf/lede的源码查看最好，以X86机型为例，源码的target/linux/x86文件夹可以看到有几个内核版本，x86文件夹里Makefile可以查看源码正在使用内核版本
 #修改版本内核（下面两行代码前面有#为源码默认最新5.4内核,没#为4.19内核,默认修改X86的，其他机型L大源码那里target/linux查看，对应修改下面的路径就好）
 #sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' ./target/linux/x86/Makefile  #修改内核版本
