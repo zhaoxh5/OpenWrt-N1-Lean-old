ec6da94c13e5f6156d4f5cf382becda2779e8c6d
zhaoxh5 <67985916+zhaoxh5@users.noreply.github.com>
2021-07-05 21:40:34 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,9 +10,10 @@ sed -i "/uci commit fstab/a\uci commit network" $ZZZ
 sed -i "/uci commit network/i\uci set network.lan.ipaddr='192.168.0.5'" $ZZZ                      # IPv4 地址(openwrt后台地址)
 sed -i "/uci commit network/i\uci set network.lan.netmask='255.255.255.0'" $ZZZ                   # IPv4 子网掩码
 sed -i "/uci commit network/i\uci set network.lan.gateway='192.168.0.1'" $ZZZ                     # IPv4 网关
-#sed -i "/uci commit network/i\uci set network.lan.broadcast='192.168.2.255'" $ZZZ                 # IPv4 广播
+#sed -i "/uci commit network/i\uci set network.lan.broadcast='192.168.2.255'" $ZZZ                # IPv4 广播
 sed -i "/uci commit network/i\uci set network.lan.dns='114.114.114.114'" $ZZZ                     # DNS(多个DNS要用空格分开)
 sed -i "/uci commit network/i\uci set network.lan.delegate='0'" $ZZZ                              # 去掉LAN口使用内置的 IPv6 管理
+echo "close_dhcp" > package/base-files/files/etc/closedhcp                                        # 关闭DHCP服务
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i 's/OpenWrt/Phicomm-N1/g' ./package/base-files/files/bin/config_generate
