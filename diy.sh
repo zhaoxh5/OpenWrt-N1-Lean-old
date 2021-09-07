bde9d2e9d21e5ae9379811b5a13abcdeb0af0769
zhaoxh5 <67985916+zhaoxh5@users.noreply.github.com>
2021-09-08 03:46:41 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,14 +9,12 @@
 sed -i 's/192.168.1.1/192.168.0.5/g' ./package/base-files/files/bin/config_generate
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
-#sed -i 's/OpenWrt/Phicomm N1/g' ./package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Phicomm N1/g' ./package/base-files/files/bin/config_generate
 
-# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
-#sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' ./package/lean/default-settings/files/zzz-default-settings
 
 #内核版本是会随着源码更新而改变的，在coolsnowwolf/lede的源码查看最好，以X86机型为例，源码的target/linux/x86文件夹可以看到有几个内核版本，x86文件夹里Makefile可以查看源码正在使用内核版本
 #修改版本内核（下面两行代码前面有#为源码默认最新5.4内核,没#为4.19内核,默认修改X86的，其他机型L大源码那里target/linux查看，对应修改下面的路径就好）
-sed -i 's/PATCHVER:=5.10/PATCHVER:=5.4/g' ./target/linux/armvirt/Makefile  #修改内核版本
+#sed -i 's/PATCHVER:=5.10/PATCHVER:=5.4/g' ./target/linux/armvirt/Makefile  #修改内核版本
 
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
@@ -24,7 +22,6 @@ git clone -b master https://github.com/vernesong/OpenClash.git package/lean/luci
 git clone -b 18.06 https://github.com/small-5/luci-app-adblock-plus.git package/lean/luci-app-adblock-plus #luci-app-adblock-plus去广告软件
 #git clone https://github.com/frainzy1477/luci-app-clash.git package/lean/luci-app-clash  #clash出国软件
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus  #京东签到
-#git clone https://github.com/project-lede/luci-app-godproxy.git package/luci-app-godproxy
 #svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/lean/luci-app-passwall  #passwall出国软件
 #svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/lean/package  #passwall出国软件配套
 svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome package/lean/luci-app-adguardhome
